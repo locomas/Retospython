7eac2d1596cba48f4e46f1896b5a61bb16f1d2c3
Sebastiangallego <locomas2013@hotmail.com>
2022-08-12 19:41:50 -0500

    Retos de la clase

---
+++ main.py
@@ -0,0 +1,38 @@
+
+#entradas del problema
+
+'''
+numero1 = None 
+numero2 = 15
+numero3 = 100
+numero3 = 80
+
+ciudad = "Medellin"
+print("tu ciudad es",ciudad)
+print(f'tu ciudad es {ciudad} y tu numero es {numero2}')
+print("El numero 1 es:  " , numero1,numero2,numero3)
+print ("el numero 2 es: " ,numero2)
+print("el numero 3 es : ",numero3)
+'''
+
+# entradas por teclado
+'''''
+nombre = input("Digite su nombre: ")
+print(f'Su nombre es {nombre}')
+'''
+'''
+numero1 = int(input('Digite el numero1: '))
+numero2 = int(input('Digite el numero2: '))
+
+print(f'el primer numero es : {numero1} y el segundo es {numero2}')
+
+suma = numero1 + numero2
+
+print(f'el resultado de la suma es {suma}')
+'''
+print("Bienvenido al programa de calculo de multiplos")
+numero1 = int(input("Digite el numero 1"))
+resultado = numero1%3
+print(f'El resultado de la division es {resultado}')
+
+
---
+++ reto2.py
@@ -0,0 +1,4 @@
+numero2 = int(input("Digite el numero2"))
+resultado = numero2%5
+
+print(f'el resultado es {resultado}')
